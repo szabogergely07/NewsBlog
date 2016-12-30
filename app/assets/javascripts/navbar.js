021e03a469572fcca803e01b96dfc9e427717d65
Greg <szabogergely07@gmail.com>
2016-12-30 10:34:05 +0100

    Testing firework background

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -7,10 +7,12 @@ function switchNavbar() {
     if (win.width() < 768) { // on mobile
         nav.removeClass('navbar-fixed-top');
         nav.addClass('navbar-fixed-bottom');
+        $("body").css("margin-top", "0px");
 
     } else { // on desktop
         nav.removeClass('navbar-fixed-bottom');
         nav.addClass('navbar-fixed-top');
+        $("body").css("margin-bottom", "0px");
         $("div.navbar-fixed-top").autoHidingNavbar();
     }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'static_pages/index'
+  get 'static_pages/contact'
+  get 'static_pages/about'
 
   root 'static_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
