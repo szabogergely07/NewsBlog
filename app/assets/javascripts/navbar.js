ff259b1582a13dab23811c18383d2fcb5b15c918
Greg <szabogergely07@gmail.com>
2017-11-23 11:53:08 +0100

    Add new template

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -4,17 +4,17 @@ var win = $(this);      // browser window
 var nav = $('.navbar'); // your navigation bar
 
 function switchNavbar() {
-    if (win.width() < 768) { // on mobile
-        nav.removeClass('navbar-fixed-top');
-        nav.addClass('navbar-fixed-bottom');
-        $("body").css("margin-top", "0px");
+  if (win.width() < 768) { // on mobile
+      nav.removeClass('navbar-fixed-top');
+      nav.addClass('navbar-fixed-bottom');
+      $("body").css("margin-top", "0px");
 
-    } else { // on desktop
-        nav.removeClass('navbar-fixed-bottom');
-        nav.addClass('navbar-fixed-top');
-        $("body").css("margin-bottom", "0px");
-        $("div.navbar-fixed-top").autoHidingNavbar();
-    }
+  } else { // on desktop
+      nav.removeClass('navbar-fixed-bottom');
+      nav.addClass('navbar-fixed-top');
+      $("body").css("margin-bottom", "0px");
+      $("div.navbar-fixed-top").autoHidingNavbar();
+  }
 }
 
 // On first load
@@ -22,7 +22,25 @@ $(function() {
     switchNavbar();
 });
 
+ size_li = $("#myList li").size();
+  x=3;
+  $('#myList li:lt('+x+')').show();
+  $('#loadMore').click(function () {
+      x= (x+1 <= size_li) ? x+1 : size_li;
+      $('#myList li:lt('+x+')').show();
+  });
+  $('#showLess').click(function () {
+      x=(x-1<0) ? 1 : x-1;
+      $('#myList li').not(':lt('+x+')').hide();
+  });
 
+
+  // Script for nav
+  $( "span.menu" ).click(function() {
+              $( ".head-nav ul" ).slideToggle(300, function() {
+              // Animation complete.
+              });
+            });
 
 });
 
