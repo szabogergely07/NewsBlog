af558fa0615e236df6ac3d41584248fa6ee5bcc0
Greg <szabogergely07@gmail.com>
2017-12-03 15:20:13 +0100

    Make contact form live with sendgrid

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,14 @@ class StaticPagesController < ApplicationController
       @articles = Article.all.order(date: :desc)
     end
   end
+
+  def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(from: @email,
+      to: 'szabogergely07@gmail.com',
+      subject: "A new contact form message from #{@name}",
+      body: @message).deliver_now
+	end
 end
